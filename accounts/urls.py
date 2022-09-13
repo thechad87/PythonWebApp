279b8f6d46d15b1498704104da31cebbb49a152a
thechad87 <cmarty87@gmail.com>
2022-09-13 15:58:33 +0200

    first push

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('customer/<str:pk>/', views.customer),
-    path('products/',views.products),
+    path('', views.home, name="home"),
     path('profile/',views.profile),
     path('dashboard/',views.dashboard, name="dashboard"),
+    path('customer/<str:pk>/', views.customer),
+    path('products/',views.products, name="products"),
     path('create_order/', views.createOrder, name ="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name ="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name ="delete_order"),
-
 
 ]
